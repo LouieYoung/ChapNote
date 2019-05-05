91fc2871ff1f29698b6d9f47a6f5e5fd129522b1
LouieYoung <438688270@qq.com>
2019-05-05 19:30:37 +0800

    bar

--- ChapNote/app/src/main/java/com/example/chapnote/MainActivity.java
+++ ChapNote/app/src/main/java/com/example/chapnote/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.chapnote;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 
@@ -13,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar=(Toolbar) findViewById(R.id.toolBar);
+        toolbar.setTitle("小书笔记"); //设置标题
+        toolbar.setLogo(R.mipmap.ic_launcher);
 
     }
 
