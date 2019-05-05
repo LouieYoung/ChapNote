f5a20f824838f47d0a11a5435d14c8ff4bc54614
LouieYoung <438688270@qq.com>
2019-05-05 22:15:04 +0800

    lyy

--- ChapNote/app/src/main/java/com/example/chapnote/MainActivity.java
+++ ChapNote/app/src/main/java/com/example/chapnote/MainActivity.java
@@ -1,23 +1,36 @@
 package com.example.chapnote;
 
-import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 
 public class MainActivity extends AppCompatActivity {
     Func f= new Func();
 
-    int black = getResources().getColor(R.color.black);
-    int back = getResources().getColor(R.color.back);
-    int green = getResources().getColor(R.color.green);
-    int purple = getResources().getColor(R.color.purple);
-    int yellow = getResources().getColor(R.color.yellow);
-    int red = getResources().getColor(R.color.red);
+//    int black = getResources().getColor(R.color.black);
+//    int back = getResources().getColor(R.color.back);
+//    int green = getResources().getColor(R.color.green);
+//    int purple = getResources().getColor(R.color.purple);
+//    int yellow = getResources().getColor(R.color.yellow);
+//    int red = getResources().getColor(R.color.red);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar=(Toolbar)findViewById(R.id.toolBar);
+
+        toolbar.setTitle("小书笔记"); //设置标题
+        toolbar.setLogo(R.mipmap.ic_launcher_round); //设置Logo
+        setSupportActionBar(toolbar); //这里注意为固定写法
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_tb,menu);
+        return true;
     }
 }
