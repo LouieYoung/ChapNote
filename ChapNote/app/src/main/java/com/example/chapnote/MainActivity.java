55452474f0c82d0f2d1e7fcc56fe8675bcc3706b
LouieYoung <438688270@qq.com>
2019-05-05 20:11:38 +0800

    lyy

--- ChapNote/app/src/main/java/com/example/chapnote/MainActivity.java
+++ ChapNote/app/src/main/java/com/example/chapnote/MainActivity.java
@@ -1,30 +1,20 @@
 package com.example.chapnote;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
+
 
 public class MainActivity extends AppCompatActivity {
     Func f= new Func();
+
+    int black = getResources().getColor(R.color.black);
+    int back = getResources().getColor(R.color.back);
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar=(Toolbar) findViewById(R.id.toolBar);
-        toolbar.setTitle("小书笔记"); //设置标题
-        toolbar.setLogo(R.mipmap.ic_launcher);
-
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main, menu);
-        return true;
-    }
-
 }
